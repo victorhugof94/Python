b07aba049ce0f6332a32842e7e07a3f2bb7f58a9
Victor Hugo <victor_hugo19_9@hotmail.com>
2021-05-13 22:20:17 -0300

    22:20

---
+++ Exercicios/Desafio038.py
@@ -0,0 +1,8 @@
+n1 = int(input('Primeiro numero: '))
+n2 = int(input('Segundo numero: '))
+if n1 > n2:
+    print('O primeiro valor é maior')
+elif n1 < n2:
+    print('O segundo valor é maior')
+else:
+    print('os valores sao iguais')
